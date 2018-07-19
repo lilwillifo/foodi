e7f4d933736a1110b72be8094a8c70425068f704
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-19 15:21:36 -0600

    authorizing heroku

--- healthapp/settings.py
+++ healthapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'foodi-tracker.herokuapp.com']
 
 
 # Application definition
