8e2bb325fe18a22d71d2a584358eccc4d0dca9f9
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-18 19:32:26 -0600

    creating users table

--- healthapp/settings.py
+++ healthapp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodi',
+    'social_django',
     'dj_database_url',
 ]
 
