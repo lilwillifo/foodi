b94a3b5a9611725b1abd43cfb08e07ae9c010627
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-20 18:29:23 -0600

    different paths for home page and user dashboard

--- foodi/views.py
+++ foodi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-
+def home(request):
+    return render(request, 'home.html')
+    
 def dashboard(request):
     return render(request, 'users/dashboard.html')
