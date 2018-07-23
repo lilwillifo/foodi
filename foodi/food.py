84cf93f7230380aa1ce7aaee07b3ef86a649caf1
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-22 18:38:37 -0600

    add units to servings and fix rounding error in percentages

--- foodi/food.py
+++ foodi/food.py
@@ -3,6 +3,7 @@ class FoodService():
         # import code; code.interact(local=dict(globals(), **locals()))
         self.name = food_data['food_name'].capitalize
         self.serving_qty = food_data['serving_qty']
+        self.serving_unit = food_data['serving_unit']
         self.calories = round(food_data['nf_calories'])
         self.total_fat = round(food_data['nf_total_fat'], 1)
         self.sat_fat = round(food_data['nf_saturated_fat'], 1)
@@ -17,29 +18,24 @@ class FoodService():
         self.dailyValueSatFat = 20
         self.dailyValueCholesterol = 300
         self.dailyValueSodium = 2400
-        self.dailyValuePotassium = 3500
-        self.dailyValuePotassium_2018 = 4700
         self.dailyValueCarb = 300
         self.dailyValueFiber = 25
         self.dailyValueCalcium = 1300
-        self.dailyValueIron = 18
-        self.dailyValueVitaminD = 20
-        self.dailyValueAddedSugar = 50
 
     def fat_percent(self):
-        return round(self.total_fat / self.dailyValueTotalFat, 2) * 100
+        return round(self.total_fat / self.dailyValueTotalFat  * 100, 2)
 
     def sat_fat_percent(self):
-        return round(self.sat_fat / self.dailyValueSatFat, 2) * 100
+        return round(self.sat_fat / self.dailyValueSatFat  * 100, 2)
 
     def cholesterol_percent(self):
-        return round(self.cholesterol / self.dailyValueCholesterol, 2) * 100
+        return round(self.cholesterol / self.dailyValueCholesterol  * 100, 2)
 
     def sodium_percent(self):
-        return round(self.sodium / self.dailyValueSodium, 2) * 100
+        return round(self.sodium / self.dailyValueSodium  * 100, 2)
 
     def carb_percent(self):
-        return round(self.carbs / self.dailyValueCarb, 2) * 100
+        return round(self.carbs / self.dailyValueCarb  * 100, 2)
 
     def fiber_percent(self):
-        return round(self.fiber / self.dailyValueFiber, 2) * 100
+        return round(self.fiber / self.dailyValueFiber  * 100, 2)
