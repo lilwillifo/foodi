27278b0cb7435c972b59313a1f63739b35ef9b4f
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-25 10:29:44 -0600

    WIP form for adding to diary

--- foodi/forms.py
+++ foodi/forms.py
@@ -1,5 +1,4 @@
-class Diary(forms.ModelForm):
-    class Meta:
-    ...
+from django import forms
+class DiaryForm(forms.Form):
+    servings = forms.ChoiceField(choices= ((str(x), x) for x in [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]))
     widgets = {'myDateField': forms.DateInput(attrs={'id': 'datepicker'})}
-    ...
--- foodi/views.py
+++ foodi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 import requests
@@ -9,6 +9,7 @@ from foodi.food_service import FoodService
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import DiaryForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
@@ -17,6 +18,22 @@ def home(request):
 
 def dashboard(request):
     return render(request, 'users/dashboard.html')
+
+def diary(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = DiaryForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return redirect('/dashboard/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        return redirect('/dashboard/')
 
 def signup(request):
     if request.method == 'POST':
@@ -34,38 +51,38 @@ def signup(request):
 
 def search(request):
     search_result = {}
-    if 'query' in request.GET:
-        food = request.GET['query']
-        url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
-        headers = {'x-app-id': config('NIX_APP_ID'),
-                   'x-app-key': config('NIX_APP_KEY'),
-                   'x-remote-user-id': '0'}
-        payload = {'query': food}
+    food = request.GET['query']
+    url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
+    headers = {'x-app-id': config('NIX_APP_ID'),
+               'x-app-key': config('NIX_APP_KEY'),
+               'x-remote-user-id': '0'}
+    payload = {'query': food}
 
-        response = requests.post(url, headers=headers, data=payload)
-        search_result = response.json()
-        raw_food = FoodService(search_result['foods'][0])
-        try:
-            food = Food.objects.get(name = raw_food.name)
-        except Food.DoesNotExist:
-            food  = Food(name = raw_food.name,
-                           img = raw_food.img['thumb'],
-                           serving_qty = raw_food.serving_qty,
-                           serving_unit = raw_food.serving_unit,
-                           calories = raw_food.calories,
-                           total_fat = raw_food.total_fat,
-                           sat_fat = raw_food.sat_fat,
-                           cholesterol = raw_food.cholesterol,
-                           sodium = raw_food.sodium,
-                           carbs = raw_food.carbs,
-                           fiber = raw_food.fiber,
-                           sugar = raw_food.sugar,
-                           protein = raw_food.protein)
-            food.save()
+    response = requests.post(url, headers=headers, data=payload)
+    search_result = response.json()
+    raw_food = FoodService(search_result['foods'][0])
+    try:
+        food = Food.objects.get(name = raw_food.name)
+    except Food.DoesNotExist:
+        food  = Food(name = raw_food.name,
+                       img = raw_food.img['thumb'],
+                       serving_qty = raw_food.serving_qty,
+                       serving_unit = raw_food.serving_unit,
+                       calories = raw_food.calories,
+                       total_fat = raw_food.total_fat,
+                       sat_fat = raw_food.sat_fat,
+                       cholesterol = raw_food.cholesterol,
+                       sodium = raw_food.sodium,
+                       carbs = raw_food.carbs,
+                       fiber = raw_food.fiber,
+                       sugar = raw_food.sugar,
+                       protein = raw_food.protein)
+        food.save()
         # end try
 
-        context = {
-            'food': food
-        }
+    context = {
+        'food': food,
+        'form': DiaryForm(),
+    }
     # import code; code.interact(local=dict(globals(), **locals()))
     return render(request, 'search.html', context)
