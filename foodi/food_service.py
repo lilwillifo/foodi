e92da86a178c2c7aa4e66b89748820d19fdea510
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-28 10:46:10 -0600

    sad path if user enters non-food

--- foodi/food_service.py
+++ foodi/food_service.py
@@ -14,29 +14,3 @@ class FoodService():
         self.fiber = round(food_data['nf_dietary_fiber'], 1)
         self.sugar = round(food_data['nf_sugars'], 1)
         self.protein = round(food_data['nf_protein'], 1)
-        self.calorieIntake = 2000
-        self.dailyValueTotalFat = 65
-        self.dailyValueSatFat = 20
-        self.dailyValueCholesterol = 300
-        self.dailyValueSodium = 2400
-        self.dailyValueCarb = 300
-        self.dailyValueFiber = 25
-        self.dailyValueCalcium = 1300
-#
-#     def fat_percent(self):
-#         return round(self.total_fat / self.dailyValueTotalFat  * 100, 2)
-#
-#     def sat_fat_percent(self):
-#         return round(self.sat_fat / self.dailyValueSatFat  * 100, 2)
-#
-#     def cholesterol_percent(self):
-#         return round(self.cholesterol / self.dailyValueCholesterol  * 100, 2)
-#
-#     def sodium_percent(self):
-#         return round(self.sodium / self.dailyValueSodium  * 100, 2)
-#
-#     def carb_percent(self):
-#         return round(self.carbs / self.dailyValueCarb  * 100, 2)
-#
-#     def fiber_percent(self):
-#         return round(self.fiber / self.dailyValueFiber  * 100, 2)
--- foodi/views.py
+++ foodi/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import auth
-from django.http import HttpResponse
 import requests
 from decouple import config
-from django.template import loader
 from foodi.models import Food, Diary, Profile
 from foodi.food_service import FoodService
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .forms import DiaryForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     # import code; code.interact(local=dict(globals(), **locals()))
@@ -64,29 +60,32 @@ def search(request):
 
     response = requests.post(url, headers=headers, data=payload)
     search_result = response.json()
-    raw_food = FoodService(search_result['foods'][0])
-    try:
-        food = Food.objects.get(name = raw_food.name)
-    except Food.DoesNotExist:
-        food  = Food(name = raw_food.name,
-                       img = raw_food.img['thumb'],
-                       serving_qty = raw_food.serving_qty,
-                       serving_unit = raw_food.serving_unit,
-                       calories = raw_food.calories,
-                       total_fat = raw_food.total_fat,
-                       sat_fat = raw_food.sat_fat,
-                       cholesterol = raw_food.cholesterol,
-                       sodium = raw_food.sodium,
-                       carbs = raw_food.carbs,
-                       fiber = raw_food.fiber,
-                       sugar = raw_food.sugar,
-                       protein = raw_food.protein)
-        food.save()
-        # end try
+    if 'foods' not in search_result:
+        return render(request, 'home.html')
+    else:
+        raw_food = FoodService(search_result['foods'][0])
+        try:
+            food = Food.objects.get(name = raw_food.name)
+        except Food.DoesNotExist:
+            food  = Food(name = raw_food.name,
+                           img = raw_food.img['thumb'],
+                           serving_qty = raw_food.serving_qty,
+                           serving_unit = raw_food.serving_unit,
+                           calories = raw_food.calories,
+                           total_fat = raw_food.total_fat,
+                           sat_fat = raw_food.sat_fat,
+                           cholesterol = raw_food.cholesterol,
+                           sodium = raw_food.sodium,
+                           carbs = raw_food.carbs,
+                           fiber = raw_food.fiber,
+                           sugar = raw_food.sugar,
+                           protein = raw_food.protein)
+            food.save()
+            # end try
 
-    context = {
-        'food': food,
-        'form': DiaryForm(initial={'food': food}),
-    }
-    # import code; code.interact(local=dict(globals(), **locals()))
-    return render(request, 'search.html', context)
+        context = {
+            'food': food,
+            'form': DiaryForm(initial={'food': food}),
+        }
+        # import code; code.interact(local=dict(globals(), **locals()))
+        return render(request, 'search.html', context)
